17de59634c968d015fec9dfa2d8a2a3cb956dd14
Giuliopime <giuliopime@gmail.com>
2022-11-10 11:11:24 +0100

    feat: lezioni mancanti

--- src/teoria/10_strutture.cpp
+++ src/teoria/10_strutture.cpp
@@ -1,3 +1,59 @@
-//
-// Created by Giulio Pimenoff Verdolin on 09/11/22.
-//
+#include "iostream"
+using namespace std;
+
+struct data {
+    int giorno, mese, anno;
+};
+
+struct persona {
+    char nome[25], cognome[25];
+    char comune_nascita[25];
+    data data_nascita;
+    enum { F, M } sesso;
+};
+
+struct conArrayDin {
+    int* arrayDin;
+};
+
+int main() {
+    persona giulio = {
+            "Giulio",
+            "Pimenoff Verdolin",
+            "Minerbe",
+            data { 29, 9, 2003 },
+            persona::M
+    };
+
+    cout << giulio.nome;
+
+    // Per puntatore
+    persona* puntatoreGiulio = &giulio;
+    puntatoreGiulio->data_nascita = data { 29, 9, 2003};
+
+    cout << puntatoreGiulio->nome;
+
+    // Posso copiare le struct
+    persona cloneDiGiulio = giulio;
+
+    // Array dinamici
+    conArrayDin cad, cad2;
+
+    cad.arrayDin = new int[giulio.data_nascita.giorno];
+    // Viene copiato il puntatore dell'array dinamico
+    // Quindi cad.arrayDin e cad2.arrayDin sono lo stesso array
+    cad2 = cad;
+
+    // Array di persone
+    int numeroPersone = 23;
+    // Molto più efficiente lavorare con puntatori
+    persona* persone[23];
+
+    for(int i=0; i<numeroPersone; i++) {
+        cout << persone[i]->nome;
+    }
+
+    return 0;
+}
+
+void ricevoStruct(persona p) {}
--- src/teoria/9_allocazione_dinamica.cpp
+++ src/teoria/9_allocazione_dinamica.cpp
@@ -1,3 +1,40 @@
-//
-// Created by Giulio Pimenoff Verdolin on 09/11/22.
-//
+#include "iostream"
+using namespace std;
+
+int main() {
+    int size;
+    cin >> size;
+
+    // Array dinamico, dichiarato con new
+    int* arrayDinamico = new int[size];
+
+    // Se il programma non ha più memoria a disposizione, l'opeatore new abortisce il programma
+    // Possiamo evitare cioò facendo restituire NULL dall'operatore new in caso la memoria sia esaurita
+    int* arrayONull = new (nothrow) int[size];
+
+    if (arrayONull != NULL) {
+        // Array valido
+    }
+
+    // Deallocazione per liberare area in memoria
+    delete[] arrayONull;
+
+    // Array dinamici multidimensionali
+    // è sostanzialmente un array di puntatori
+    int** multiDimArray = new int* [size];
+    int sizeRighe = 5;
+    for(int i=0; i<size; i++) {
+        multiDimArray[i] = new int[sizeRighe];
+    }
+
+    return 0;
+}
+
+int* restituisciArray() {
+    int size;
+    cin >> size;
+
+    int* array = new int[size];
+
+    return array;
+}
