79a026135b94021f4c8cdabfbd51f95739bb00f5
Giuliopime <giuliopime@gmail.com>
2023-01-19 19:16:07 +0100

    esercizi

--- src/possibile-esame-2/3/esercizio3.cc
+++ src/possibile-esame-2/3/esercizio3.cc
@@ -7,7 +7,7 @@ using namespace std;
 
 int decodificaNumero(char numeroCodificato []);
 
-int main() {
+int main() { 
     int dimensioneMassima = 9;
     char numeroCodificato [dimensioneMassima];
     cout << "Inserisci l'array: ";
@@ -16,9 +16,37 @@ int main() {
     cout << "Decodifica: " << decodifica << endl;
 }
 
+
 int decodificaNumero(char numeroCodificato []) {
-    // Scrivere il codice qui
-    
-    // Potete assumere che l'array rispetti le assunzioni
-    // dell'esercizio (non serve fare controlli sull'input)
+    init();
+
+    int decodifica = 0;
+    int lunghezzaNumeroCodificato = strlen(numeroCodificato);
+    int esponente = lunghezzaNumeroCodificato;
+ 
+    for (int i = 0; i <= lunghezzaNumeroCodificato; i++) {
+       cout << "Pusho " << (i + 1) << endl;
+        push(i + 1);
+ 
+        if (i == lunghezzaNumeroCodificato || numeroCodificato[i] == 'I') {
+            while (!vuota()) {
+                int temp;
+                top(temp);
+                cout << "temp: " << temp << endl;
+
+                cout << "Aggiungo a decodifica: " << (temp * pow(10, esponente)) << " (pow: " << pow(10, esponente) << ")" << endl;
+                decodifica = decodifica + temp * pow(10, esponente);
+                cout << "Decodifica: " << decodifica << endl;
+                esponente--;
+                cout << "Esponente: " << esponente << endl;
+                pop();
+            }
+        }
+
+        cout << endl;
+    }
+ 
+    cout << "Fine" << endl;
+    deinit();
+    return decodifica;
 }
